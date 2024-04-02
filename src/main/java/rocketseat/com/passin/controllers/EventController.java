d3f3aa51a35dfe342b784cc437928265da489b05
Christian Ricci <riccichristian338@gmail.com>
2024-04-02 09:32:23 -0300

    FEAT: Add Event Service

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -1,16 +1,23 @@
 package rocketseat.com.passin.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rocketseat.com.passin.services.EventService;
 
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
 
-    @GetMapping
-    public ResponseEntity<String> getTeste(){
+    private final EventService service;
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity<String> getEvent(@PathVariable String eventId){
+        this.service.getEventDetail(eventId);
         return ResponseEntity.ok("sucesso");
     }
 }
---
+++ src/main/java/rocketseat/com/passin/services/EventService.java
@@ -0,0 +1,51 @@
+package rocketseat.com.passin.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rocketseat.com.passin.domain.attendee.Attendee;
+import rocketseat.com.passin.domain.event.Event;
+import rocketseat.com.passin.dto.event.EventIdDTO;
+import rocketseat.com.passin.dto.event.EventRequestDTO;
+import rocketseat.com.passin.dto.event.EventResponseDTO;
+import rocketseat.com.passin.repositorys.AttendeeRepository;
+import rocketseat.com.passin.repositorys.EventRepository;
+
+import java.text.Normalizer;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+
+    private final EventRepository eventRepository;
+    private final AttendeeRepository attendeeRepository;
+
+    public EventResponseDTO getEventDetail(String eventId){
+        Event event = this.eventRepository.findById(eventId).orElseThrow(
+                () -> new RuntimeException("Event not found with ID: "+eventId)
+        );
+        List<Attendee> attendeeList = this.attendeeRepository.findByEventId(eventId);
+        return new EventResponseDTO(event, attendeeList.size());
+    }
+
+    public EventIdDTO createEvent(EventRequestDTO eventDTO){
+        Event newEvent = new Event();
+        newEvent.setTitle(eventDTO.title());
+        newEvent.setDetails(eventDTO.details());
+        newEvent.setMaximumAttendees(eventDTO.maximumAttendees());
+        newEvent.setSlug(this.createSlug(eventDTO.title()));
+
+        this.eventRepository.save(newEvent);
+
+        return new EventIdDTO(newEvent.getId());
+    }
+
+    private String createSlug(String text){
+        String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return normalized.replaceAll("[\\p{InCOMBINING_DIACRITICAL_MARKS}]","")
+                .replaceAll("[^\\w\\s]","")
+                .replaceAll("\\s+","-")
+                .toLowerCase();
+    }
+}
